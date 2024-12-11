0e1caab0fb5ca8bc31ee334cccce04b1a6bc6c37
cgilsanchez <cgilsanchez.02@gmail.com>
2024-12-11 19:51:52 +0100

    elegir chef

--- AngularReceteo/src/app/comoponents/modal/modal.component.ts
+++ AngularReceteo/src/app/comoponents/modal/modal.component.ts
@@ -1,34 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { RecipesService } from '../../service/recipes.service';
+import { ChefService, Chef } from '../../service/chefs.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {
-  @Input() recipe: any = { name: '', ingredients: '', descriptions: '' }; // Cambiado a descriptions
+export class ModalComponent implements OnInit {
+  @Input() recipe: any = {
+    name: '',
+    ingredients: '',
+    description: '',
+    chef: null, // ID del chef asignado
+  };
+  chefs: Chef[] = []; // Lista de chefs disponibles
 
   constructor(
-    private modalCtrl: ModalController,
-    private recipesService: RecipesService
+    private modalController: ModalController,
+    private chefService: ChefService
   ) {}
 
-  saveRecipe() {
-    console.log('Datos enviados al guardar:', this.recipe); // Verifica que descriptions tenga valor
-    this.recipesService.saveRecipe(this.recipe).subscribe({
-      next: () => {
-        console.log('Receta guardada con éxito');
-        this.modalCtrl.dismiss(true); // Cerrar modal y notificar cambios
-      },
-      error: (err) => {
-        console.error('Error al guardar la receta:', err);
-      }
+  ngOnInit() {
+    // Cargar la lista de chefs al abrir el modal
+    this.chefService.getChefs().subscribe((data) => {
+      this.chefs = data;
     });
   }
 
   close() {
-    this.modalCtrl.dismiss(false); // Cerrar modal sin guardar
+    this.modalController.dismiss();
+  }
+
+  saveRecipe() {
+    // Aquí envías los datos de la receta, incluyendo el chef seleccionado
+    console.log(this.recipe);
+    this.modalController.dismiss(this.recipe);
   }
 }
--- AngularReceteo/src/app/service/chefs.service.ts
+++ AngularReceteo/src/app/service/chefs.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Chef {
+  id: number;
+  name: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ChefsService {
+export class ChefService {
+  private baseUrl = 'http://localhost:1337/api/chefs';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getChefs(): Observable<Chef[]> {
+    return this.http.get<Chef[]>(`${this.baseUrl}/chefs`);
+  }
 }
