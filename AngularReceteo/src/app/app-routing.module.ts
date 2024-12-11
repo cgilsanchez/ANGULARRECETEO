4eeb0701e69e7f023ad9576185e57fa74ee3b1f0
cgilsanchez <cgilsanchez.02@gmail.com>
2024-12-11 22:22:46 +0100

    menu hamburguesa

--- AngularReceteo/src/app/app-routing.module.ts
+++ AngularReceteo/src/app/app-routing.module.ts
@@ -3,38 +3,43 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
+    loadChildren: () =>
+      import('./pages/register/register.module').then((m) => m.RegisterPageModule),
   },
   {
-    path: 'recetas',
-    loadChildren: () => import('./pages/recetas/recetas.module').then(m => m.RecetasPageModule)
+    path: 'recetas', // Ruta para la página de recetas
+    loadChildren: () =>
+      import('./pages/recetas/recetas.module').then((m) => m.RecetasPageModule),
   },
   {
-    path: 'chef',
-    loadChildren: () => import('./pages/chef/chef.module').then(m => m.ChefPageModule)
+    path: 'chefs', // Ruta para la página de chefs
+    loadChildren: () =>
+      import('./pages/chef/chef.module').then((m) => m.ChefPageModule),
   },
   {
-    path: 'usuario',
-    loadChildren: () => import('./pages/usuario/usuario.module').then(m => m.UsuarioPageModule)
-  }
+    path: 'usuarios', // Ruta para la página de usuarios
+    loadChildren: () =>
+      import('./pages/usuario/usuario.module').then((m) => m.UsuarioPageModule),
+  },
+  {
+    path: '', // Redirigir al login como página inicial
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**', // Ruta por defecto si no encuentra la página
+    redirectTo: 'login',
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-
-
+export class AppRoutingModule {}
